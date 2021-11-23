8de8ec0b51273b1a8b5b78c94a8143db9a32fe9f
maheshRivaanInfo <mahesh@technoproject.in>
2021-11-23 15:26:53 +0530

    admin side order list

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -181,7 +181,7 @@ const Dashboard = (props) => {
               orders.results.length &&
               orders.results.map((itm) => (
       <tr>
-        <td>{itm.resto_name[0].name}</td>
+        <td>{/*itm.resto_name[0].name*/}</td>
         <td>{moment(itm.createdAt).format("DD/MM/YYYY")} {moment(itm.createdAt).format("HH:mm")}</td>
         <td>{itm.orderNo}</td>
         <td>{itm.tableNo || "NA"}</td>
